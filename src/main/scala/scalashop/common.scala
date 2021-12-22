31a71f2a788f7289f6bf74aa2bed3eb24bfb0504
Viktor Chukhantsev <email@viktor.dev>
2021-12-22 13:52:15 +0700

    Add box blur method.

--- src/main/scala/scalashop/common.scala
+++ src/main/scala/scalashop/common.scala
@@ -21,8 +21,7 @@ def blue(c: RGBA): Int = (0x0000ff00 & c) >>> 8
 def alpha(c: RGBA): Int = (0x000000ff & c) >>> 0
 
 /** Used to create an RGBA value from separate components. */
-def rgba(r: Int, g: Int, b: Int, a: Int): RGBA =
-  (r << 24) | (g << 16) | (b << 8) | (a << 0)
+def rgba(r: Int, g: Int, b: Int, a: Int): RGBA = (r << 24) | (g << 16) | (b << 8) | (a << 0)
 
 /** Restricts the integer into the specified range. */
 def clamp(v: Int, min: Int, max: Int): Int =
@@ -38,16 +37,27 @@ class Img(val width: Int, val height: Int, private val data: Array[RGBA]):
 
 /** Computes the blurred RGBA value of a single pixel of the input image. */
 def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA =
-  for(i <- 0 until radius)
-    for(j <- 0 until radius)
-      yield src(x + i, y + j)
-  // TODO implement using while loops
-  ???
+  val pixels: List[RGBA] =
+    (for
+      i <- 0 until radius
+      j <- 0 until radius
+      xx = clamp(x + i, 0, src.width)
+      yy = clamp(y + j, 0, src.height)
+    yield src(xx, yy)).toList
+
+  val len = pixels.length
+  val r = pixels.map(red(_)).sum / len
+  val g = pixels.map(green(_)).sum / len
+  val b = pixels.map(blue(_)).sum / len 
+  val a = pixels.map(alpha(_)).sum / len
+
+  rgba(r, g, b, a)
 
 val forkJoinPool = ForkJoinPool()
 
 abstract class TaskScheduler:
   def schedule[T](body: => T): ForkJoinTask[T]
+
   def parallel[A, B](taskA: => A, taskB: => B): (A, B) =
     val right = task {
       taskB
@@ -56,29 +66,26 @@ abstract class TaskScheduler:
     (left, right.join())
 
 class DefaultTaskScheduler extends TaskScheduler:
+
   def schedule[T](body: => T): ForkJoinTask[T] =
-    val t = new RecursiveTask[T] {
-      def compute = body
-    }
+    val t =
+      new RecursiveTask[T] {
+        def compute = body
+      }
     Thread.currentThread match
-      case wt: ForkJoinWorkerThread =>
-        t.fork()
-      case _ =>
-        forkJoinPool.execute(t)
+      case wt: ForkJoinWorkerThread => t.fork()
+      case _                        => forkJoinPool.execute(t)
     t
 
-val scheduler =
-  DynamicVariable[TaskScheduler](DefaultTaskScheduler())
+val scheduler = DynamicVariable[TaskScheduler](DefaultTaskScheduler())
 
-def task[T](body: => T): ForkJoinTask[T] =
-  scheduler.value.schedule(body)
+def task[T](body: => T): ForkJoinTask[T] = scheduler.value.schedule(body)
 
-def parallel[A, B](taskA: => A, taskB: => B): (A, B) =
-  scheduler.value.parallel(taskA, taskB)
+def parallel[A, B](taskA: => A, taskB: => B): (A, B) = scheduler.value.parallel(taskA, taskB)
 
 def parallel[A, B, C, D](taskA: => A, taskB: => B, taskC: => C, taskD: => D): (A, B, C, D) =
-  val ta = task { taskA }
-  val tb = task { taskB }
-  val tc = task { taskC }
+  val ta = task(taskA)
+  val tb = task(taskB)
+  val tc = task(taskC)
   val td = taskD
   (ta.join(), tb.join(), tc.join(), td)
--- src/test/scala/scalashop/BlurSuite.scala
+++ src/test/scala/scalashop/BlurSuite.scala
@@ -4,5 +4,12 @@ import java.util.concurrent.*
 import scala.collection.*
 
 class BlurSuite extends munit.FunSuite:
-  // Put tests here
-  ()
+  test("boxBlurKernel") {
+    val canvas = new PhotoCanvas
+    canvas.reload()
+    val image = canvas.image
+
+    val obtained = boxBlurKernel(image, 2, 2, 2)
+    val expected = -15856112
+    assertEquals(obtained, expected)
+  }
